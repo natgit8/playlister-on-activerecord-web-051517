caab30be6c857294a440ca046d0153112792ce6d
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-05-25 17:21:40 -0400

    Done.

--- db/migrate/01_create_songs.rb
+++ db/migrate/01_create_songs.rb
@@ -1,2 +1,10 @@
 class CreateSongs < ActiveRecord::Migration
+
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.references :artist
+      t.references :genre 
+    end
+  end
 end
